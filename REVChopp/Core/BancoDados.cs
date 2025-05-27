6514817493b7bdadd1234fd2bc907cfd849dd8c8
heitorsinistro <heitorcabral.cartoon@gmail.com>
2025-05-26 23:43:23 -0300

    reestruturação do banco de dados

---
+++ REVChopp/Core/BancoDados.cs
@@ -0,0 +1,27 @@
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace REVChopp.Core
+{
+    public static class BancoDados
+    {
+        private static readonly string ConnectionString;
+
+        static BancoDados()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+            ConnectionString = configuration.GetConnectionString("RevChopp");
+        }
+
+        public static MySqlConnection ObterConexao()
+        {
+            var conexao = new MySqlConnection(ConnectionString);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
--- REVChopp/Program/Program.cs
+++ REVChopp/Program/Program.cs
@@ -1,14 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using REVChopp.Core;
-using REVChopp.Data;
-
-var repo = new UsuarioRepository();
-var usuarios = repo.ListarUsuarios();
-
-foreach (var u in usuarios)
-{
-    Console.WriteLine($"{u.Id}: {u.Nome} ({u.NivelAcesso})");
-}
-
-Sistema.MenuPrincipal();
+﻿
