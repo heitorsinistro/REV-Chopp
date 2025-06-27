a79099fe976e19c4596cfd3c8c2d29d03a387dbc
heitorsinistro <heitorcabral.cartoon@gmail.com>
2025-06-26 21:28:11 -0300

    Update assembly informational version and cache file for consistency

--- REVChopp/obj/Debug/net9.0/REVChopp.AssemblyInfo.cs
+++ REVChopp/obj/Debug/net9.0/REVChopp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("REVChopp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea0c31be80bb9a1aa18fe6b88814ee83af24728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c956fada9b654a363b574e52b60067bebee79256")]
 [assembly: System.Reflection.AssemblyProductAttribute("REVChopp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("REVChopp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
